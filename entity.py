6f9a6cc344be9bc80be385f52700ae0780b59fc9
lv_oz2 <65639555+lvoz2@users.noreply.github.com>
2024-05-27 20:35:59 +1000

    Create entity.py

---
+++ entity.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Entity():
+    def __init__(self, surf: pygame.Surface, x: int, y: int, window: pygame.Surface, visible: boolean=False, scale: int=1) -> None:
+        self.surf = surf.convert_alpha()
+        self.x = x
+        self.y = y
+        self.window = window
+        self.height = self.surf.get_height()
+        self.width = self.surf.get_width()
+        self.visible = visible
+
+    def get_opp_corner(self) -> list[int]:
+        return [self.x + self.width, self.y + self.height]
+
+    def draw(self) -> None:
+        if self.visible:
+            if ((0 - self.width) < self.x < self.window.get_width()) and ((0 - self.height) < self.y < self.window.get_height()):
+                self.window.blit(self.surf, [self.x, self.y])
+            else:
+                self.visible = False
