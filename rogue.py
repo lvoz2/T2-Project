980f7930cae69579854892540a015aba90798c66
lvoz2 <lv_oz2@icloud.com>
2024-05-29 17:06:39 +1000

    Integrated rogue class into new system

--- rogue.py
+++ rogue.py
@@ -1,6 +1,6 @@
 from character import Character
 
 class Rogue(Character):
-    def __init__(self, name):
-        super().__init__(name, "Rogue", armour = 7)
+    def __init__(self, x: int, y: int, window: pygame.Surface, name: str, scale: int=1):
+        super().__init__(GAME_ASSETS["rogue"], x, y, window, name, "rogue", scale, defense=15)
         # Additional attributes and methods specific to the Rogue class
